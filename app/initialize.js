2cb542241ebbca83b83c31b45e76a3c780b42f43
Javi <suavejavi@gmail.com>
2019-03-16 17:05:56 -0400

    Add fetch api

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
-  console.log('Initialized app');
+
+document.getElementById('form').addEventListener('submit', postData);
+
+function postData(event) {
+  event.preventDefault();
+
+  let tittle = document.getElementById('firstName').value;
+  let body = document.getElementById('lastName').value;
+
+  fetch('https://i-approve-tadhack.herokuapp.com/approvals/api', {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify({ tittle: tittle, body: body })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log('error', err));
+}
+console.log('Initialized app, testing');
 });
