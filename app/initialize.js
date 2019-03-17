9428f09c9e36252589bb1a8249f0289744f3bdea
Javi <suavejavi@gmail.com>
2019-03-16 23:44:49 -0400

    Add logo and border image

--- app/initialize.js
+++ app/initialize.js
@@ -9,10 +9,14 @@ function postData(event) {
   let tittle = document.getElementById('firstName').value;
   let body = document.getElementById('lastName').value;
 
-  fetch('https://i-approve-tadhack.herokuapp.com/approvals/api', {
+  fetch('http://ed42799e.ngrok.io/approvals/api', {
     method: 'POST',
-    headers: new Headers(),
-    body: JSON.stringify({ 'approval[media_url]': tittle, 'approval[approver_number]': '14074531225' })
+    headers: new Headers(
+      {'Content-Type': 'application/json'}
+    ),
+    body: JSON.stringify({ 'approval': { media_url: tittle,
+      approver_number: '14074531225'
+                                        }})
   })
     .then(res => res.json())
     .then(data => console.log(data))
