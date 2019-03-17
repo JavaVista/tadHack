49298ef854d629cc31edfc73b918ad42d6cbd326
Javi <suavejavi@gmail.com>
2019-03-17 00:27:21 -0400

    Fix logo

--- app/initialize.js
+++ app/initialize.js
@@ -1,40 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
 
-document.getElementById('form').addEventListener('submit', postData);
+  document.getElementById('form').addEventListener('submit', postData);
 
-function postData(event) {
-  event.preventDefault();
-
-  let tittle = document.getElementById('firstName').value;
-  let body = document.getElementById('lastName').value;
-
-  fetch('http://ed42799e.ngrok.io/approvals/api', {
-    method: 'POST',
-    headers: new Headers(
-      {'Content-Type': 'application/json'}
-    ),
-    body: JSON.stringify({ 'approval': { media_url: tittle,
-      approver_number: '14074531225'
-                                        }})
-  })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log('error', err));
-}
-console.log('Initialized app, this is my test');
-
-function previewFile() {
-  var preview = document.querySelector('img');
-  var file    = document.querySelector('input[type=file]').files[0];
-  var reader  = new FileReader();
-
-  reader.addEventListener("load", function () {
-    preview.src = reader.result;
-  }, false);
-
-  if (file) {
-    reader.readAsDataURL(file);
+  function postData(event) {
+    //event.preventDefault();
+    let body = document.getElementById('form').value;
+    fetch('http://ed42799e.ngrok.io/approvals/api', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify({
+        approval: { media_url: body,
+           approver_number: '14074531225'
+           }
+      })
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log('error', err));
   }
-}
+
+  const previewFile = function(e) {
+    let output = document.getElementById('output');
+    output.src = URL.createObjectURL(e.target.files[0]);
+  };
 });
